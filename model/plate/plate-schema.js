cea2ea3ccc6d13f932f684f90eb44ae500204fa9
Edgar Baeza <ebaeza1992@gmail.com>
2016-09-11 01:41:14 -0500

    Se agrega uuidCamera al modelo de PLate

--- model/plate/plate-schema.js
+++ model/plate/plate-schema.js
@@ -4,8 +4,9 @@ const Schema   = mongoose.Schema;
 
 const plateSchema = new Schema({
   plate: { type: String, required: true },
-  confidence:  { type: Number },
-  capturedTime: { type: Date },
+  confidence:  { type: Number, required: true },
+  capturedTime: { type: Date, required: true },
+  uuidCamera: { type: String, required: true },
   createTime: { type: Date, default: Date.now }
 });
 
